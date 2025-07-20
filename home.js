de2442d3f5d2ca8b94b74bab0c8aebd31e22b5d9
Nacho1240 <damian.elizondo@mail.udp.cl>
2025-07-20 16:11:50 -0400

    Arreglo de bux menores con el navbar

--- home.js
+++ home.js
@@ -3,14 +3,14 @@ document.addEventListener("DOMContentLoaded", function () {
     let sidebar = document.getElementById("sidebar");
     let container = document.getElementById("menu-btn-container");
     let horizontalBar = document.querySelector('.navbar');
+    let navegationLinks = document.querySelectorAll('.navigation-link');
 
-  
     if (!menuButton || !sidebar || !container || !horizontalBar) {
         console.error("Uno o más elementos no se han encontrado en el DOM.");
         return;
     }
 
-    // Función para verificar el espacio de la barra horizontal, esta funcionando bien
+    // Función para verificar el espacio de la barra horizontal
     function checkHorizontalBarSpace() {
         if (horizontalBar.scrollWidth > horizontalBar.clientWidth || horizontalBar.offsetWidth <= 600) {
             container.classList.add('horizontal-bar-active'); 
@@ -18,6 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
             container.classList.remove('horizontal-bar-active');
         }
     }
+
     document.addEventListener("click", function (event) {
         const isClickInsideSidebar = sidebar.contains(event.target);
         const isClickOnMenuButton = menuButton.contains(event.target);
@@ -26,11 +27,17 @@ document.addEventListener("DOMContentLoaded", function () {
             sidebar.classList.remove("active");
         }
     });
+
+    // Cerrar sidebar al hacer clic en un link de navegación
+    navegationLinks.forEach(link => {
+        link.addEventListener("click", function () {
+            sidebar.classList.remove("active");
+        });
+    });
+
     // Comprobar al cargar la página
     checkHorizontalBarSpace();
 
     // Comprobar cada vez que el tamaño de la ventana cambie
     window.addEventListener('resize', checkHorizontalBarSpace);
-
-
 });
